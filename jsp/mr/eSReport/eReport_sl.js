e05bcbd9590f7cf72be73816c7b727380e458c79
shuleihust <578968229@qq.com>
2014-11-13 01:14:18 +0800

    report pages

--- jsp/mr/eSReport/eReport_sl.js
+++ jsp/mr/eSReport/eReport_sl.js
@@ -18,8 +18,36 @@ $(function(){
 		$('.part4').hide();
 		$('.part3').hide();
 		$('.part5').show();
+		$('.page2').show();
 	});
 	$('.part4 .close,.part2 .close').click(function(){
 		$('.part4').hide();
+	});
+	
+	$('.cell_edit').each(function(i,n){
+		$(n).html($('<input type="text" class="cell_input"/>').val($(n).text()));
+	});
+	$('.action .model').click(function(){
+		if($(this).text()=='数据'){
+			$('.action .model').each(function(i,n){
+				$(n).text('格式');
+			});
+		}else{
+			$('.action .model').each(function(i,n){
+				$(n).text('数据');
+			});
+		}
+	});
+	$('.page1').click(function(){
+		$('.part5').hide();
+		$('.part3').show();
+		$('.page1').addClass('active');
+		$('.page2').removeClass('active');
+	});
+	$('.page2').click(function(){
+		$('.part3').hide();
+		$('.part5').show();
+		$('.page2').addClass('active');
+		$('.page1').removeClass('active');
 	});
 });
